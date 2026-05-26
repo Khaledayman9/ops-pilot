1be5b53396d70d5003debd6e93d370d677c126cd
khaledayman9 <khaledayman012@gmail.com>
2026-05-27 02:01:38 +0300

    chore(db): added first migration version

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import os
 from logging.config import fileConfig
 
 from alembic import context
@@ -13,7 +14,8 @@ from app.db.postgres import Base
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.POSTGRES_URL)
+db_url = os.environ.get("ALEMBIC_DATABASE_URL") or settings.POSTGRES_URL
+config.set_main_option("sqlalchemy.url", db_url)
 
 if config.config_file_name:
     fileConfig(config.config_file_name)
@@ -21,37 +23,34 @@ if config.config_file_name:
 target_metadata = Base.metadata
 
 
-def run_migrations_offline():
+def run_migrations_offline() -> None:
     context.configure(
         url=config.get_main_option("sqlalchemy.url"),
         target_metadata=target_metadata,
         literal_binds=True,
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
 
-def do_run_migrations(connection):
+def do_run_migrations(connection) -> None:
     context.configure(connection=connection, target_metadata=target_metadata)
     with context.begin_transaction():
         context.run_migrations()
 
 
-async def run_async_migrations():
+async def run_async_migrations() -> None:
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=NullPool,
     )
-
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
-
     await connectable.dispose()
 
 
-def run_migrations_online():
+def run_migrations_online() -> None:
     asyncio.run(run_async_migrations())
 
 
